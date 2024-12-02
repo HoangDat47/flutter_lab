c9b2349b98fa5c9fc2faa021a4b7bb4c24afa787
HoangDat47 <dat47707@gmail.com>
2024-12-02 08:56:51 +0700

    Update main function

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:flutter_lab/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const IAmRich());
+    await tester.pumpWidget(const NavigationRailPage());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
